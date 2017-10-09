a88b15e1661d74299b0f3335068a31a2249a779b
IfJosean <josean7jkami@gmail.com>
2017-10-09 12:35:13 +0200

    singleton

---
+++ src/main/java/I18N.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Josean on 09/10/2017.
+ */
+public class I18N {
+
+    String data;
+
+    private I18N instance;
+    private I18N (){
+        data = new
+    }
+
+
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Josean on 09/10/2017.
+ */
+public class Utils {
+
+    public class Singleton
+}
